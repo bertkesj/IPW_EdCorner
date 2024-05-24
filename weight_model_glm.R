388ac11be8181e046332b876d953ed9c35559a29
Alex Keil <alexpkeil1@users.noreply.github.com>
2024-05-24 12:13:14 -0400

    skeleton functions
    added in some candidate file placeholders and skeleton functions. The idea is to design around a simple user interface function, and fill in actual functions to facilitate use-ability. Also gave example of roxygen syntax for package documentation development (not sure if you used that for LTASR, but it's super handy)

---
+++ weight_model_glm.R
@@ -0,0 +1,8 @@
+##################################################################
+# weight_model_glm.R
+#  Internal functions for fitting models for weights
+#  Functions: 
+##################################################################
+
+#estimate_confounding_weights
+#estimate_censoring_weights
